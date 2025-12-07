aa58bb6c8657d67faf1440be2a456ac024709fce
sophia123-boop <sofiew0504@gmail.com>
2025-12-07 18:27:06 -0500

    Add files via upload

--- project4 - pygame/unit9/hw.py
+++ project4 - pygame/unit9/hw.py
@@ -12,10 +12,16 @@ height = 600
 
 # starting coordinates of box
 box_size = 30
+
+# box 1 coordinates
 box_x = 0
 box_y = 0
+
+# box 2 coordinates
 box_x2 = width - box_size
 box_y2 = height - box_size
+
+# initializing boxes
 rect1 = pygame.Rect(box_x, box_y, 30, 30)
 rect2 = pygame.Rect(box_x2, box_y2, 30, 30)
 
@@ -24,20 +30,24 @@ screen = pygame.display.set_mode((width, height))
 
 clock = pygame.time.Clock()
 while True:
-    clock.tick(30)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    clock.tick(30) # 30 frames per second
+    for event in pygame.event.get(): # if user clicked quit button
+        if event.type == pygame.QUIT: # exit
             exit()
 
+    # if pink box touches edge, stop
     if box_x + box_size > width:
         box_x = width - box_size
 
+    # if green box touches edge, stop
     if box_x2 < 0:
         box_x2 = 0
-    # make the box move to the right
+
+    # make the boxes move to the right and left
     box_x += 5
     box_x2 -= 5
 
+    # drawing the boxes
     rect1 = pygame.Rect(box_x, box_y, 30, 30)
     rect2 = pygame.Rect(box_x2, box_y2, 30, 30)
     pygame.draw.rect(screen, pink, rect1)
